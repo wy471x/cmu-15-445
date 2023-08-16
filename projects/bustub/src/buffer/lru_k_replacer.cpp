2a998f2502b716d7a31d8fd5a0e7ddd0088e503f
wy471x <wy471x@gmail.com>
2023-08-16 23:02:06 +0800

    project2 finishing.

--- projects/bustub/src/buffer/lru_k_replacer.cpp
+++ projects/bustub/src/buffer/lru_k_replacer.cpp
@@ -16,9 +16,49 @@ namespace bustub {
 
 LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
 
-auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
+auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
+  for (auto it = less_than_k.begin(); it != less_than_k.end(); it++) {
+    if (*frame_id == it->GetFrameId() && it->IsEvictable()) {
+      less_than_k.erase(it);
+      less_than_k_map.erase(less_than_k_map.find(it->GetFrameId()));
+      return true;
+    }
+  }
 
-void LRUKReplacer::RecordAccess(frame_id_t frame_id) {}
+  for (auto it = greater_than_eq_k.begin(); it != greater_than_eq_k.end(); it++) {
+    if (*frame_id == it->GetFrameId() && it->IsEvictable()) {
+      greater_than_eq_k.erase(it);
+      greater_than_eq_k_map.erase(greater_than_eq_k_map.find(it->GetFrameId()));
+      return true;
+    }
+  }
+  return false;
+}
+
+void LRUKReplacer::RecordAccess(frame_id_t frame_id) {
+  BUSTUB_ASSERT(frame_id > replacer_size_, "Invalid frame id!");
+
+  for (auto it = less_than_k.begin(); it != less_than_k.end(); it++) {
+    if (frame_id == it->GetFrameId()) {
+      it->IncrementUsedCnt();
+      if (it->GetUsedCnt() >= k_) {
+        greater_than_eq_k.push_back(std::make_unique<Frame>(it));
+        greater_than_eq_k_map.insert(std::make_pair(frame_id, greater_than_eq_k));
+      } else {
+        less_than_k.push_back(std::make_unique<Frame>(it));
+      }
+      less_than_k.erase(it);
+    }
+  }
+
+  for (auto it = greater_than_eq_k.begin(); it != greater_than_eq_k.end(); it++) {
+    if (frame_id == it->GetFrameId()) {
+      it->IncrementUsedCnt();
+      greater_than_eq_k.push_back(std::make_unique<Frame>(it));
+      greater_than_eq_k.erase(it);
+    }
+  }
+}
 
 void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
 
--- projects/bustub/src/include/buffer/lru_k_replacer.h
+++ projects/bustub/src/include/buffer/lru_k_replacer.h
@@ -132,14 +132,52 @@ class LRUKReplacer {
    */
   auto Size() -> size_t;
 
+ 
+ class Frame {
+  public:
+    explicit Frame(frame_id_t frame_id) {
+      frame_id_ = frame_id;
+    }
+
+    inline auto GetUsedCnt() -> const int {
+      return used_cnt_;
+    }
+
+    inline auto IsEvictable() -> bool {
+      return evictable_;
+    }
+
+    inline auto GetFrameId() -> const frame_id_t {
+      return frame_id_;
+    }
+
+    inline auto SetAccessTimestamp(size_t current_timestamp) -> void {
+      access_timestamp_ = current_timestamp;
+    }
+
+    inline auto IncrementUsedCnt() -> void {
+      used_cnt_++;
+    }
+
+  private:
+    int used_cnt_;
+    bool evictable_;
+    frame_id_t frame_id_;
+    size_t access_timestamp_;
+ };
+
  private:
   // TODO(student): implement me! You can replace these member variables as you like.
   // Remove maybe_unused if you start using them.
-  [[maybe_unused]] size_t current_timestamp_{0};
-  [[maybe_unused]] size_t curr_size_{0};
-  [[maybe_unused]] size_t replacer_size_;
-  [[maybe_unused]] size_t k_;
+  size_t current_timestamp_{0};
+  // [[maybe_unused]] size_t curr_size_{0};
+  size_t replacer_size_;
+  size_t k_;
   std::mutex latch_;
+  std::list<std::unique_ptr<Frame>> greater_than_eq_k;
+  std::list<std::unique_ptr<Frame>> less_than_k;
+  std::unordered_map<frame_id_t, std::list<std::unique_ptr<Frame>>::iterator> greater_than_eq_k_map;
+  std::unordered_map<frame_id_t, std::list<std::unique_ptr<Frame>>::iterator> less_than_k_map;
 };
 
 }  // namespace bustub
