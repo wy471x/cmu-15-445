e7ed0cd446df0d1af084aa3d27f41b620c03af28
wy471x <wy471x@gmail.com>
2023-10-19 22:57:44 +0800

    project2 part1 finishing.

--- projects/bustub/src/storage/page/b_plus_tree_page.cpp
+++ projects/bustub/src/storage/page/b_plus_tree_page.cpp
@@ -52,7 +52,19 @@ void BPlusTreePage::SetMaxSize(int size) { max_size_ = size; }
  * Helper method to get min page size
  * Generally, min page size == max page size / 2
  */
-auto BPlusTreePage::GetMinSize() const -> int { return max_size_ / 2; }
+auto BPlusTreePage::GetMinSize() const -> int { 
+    // if cur page is root page.
+    if (IsRootPage()) {
+        return IsLeafPage() ? 1 : 2;
+    } 
+
+    // if cur page is leaf page.
+    if (IsLeafPage()) {
+        return std::ceil(max_size_ - 1 / 2);
+    }
+
+    return std::ceil(max_size_ / 2) - 1;
+}
 
 /*
  * Helper methods to get/set parent page id
